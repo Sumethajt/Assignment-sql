c646dea39ae69362f36906ef531609044a42ad59
Sumethajt <167110082+Sumethajt@users.noreply.github.com>
2025-04-05 20:45:28 +0530

    Create DatabaseConnectionException.java

---
+++ Case_Study/Cs_java/exceptopns/DatabaseConnectionException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class DatabaseConnectionException extends Exception {
+    public DatabaseConnectionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
