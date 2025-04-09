d10eefe01e50410e58a363a7bfcf2b431fcb62ee
Sumethajt <167110082+Sumethajt@users.noreply.github.com>
2025-04-09 12:05:05 +0530

    Add files via upload

---
+++ JavaCodingChallenge/PetPals/entities/Donation.java
@@ -0,0 +1,29 @@
+package entities;
+
+public abstract class Donation {
+    protected String donorName;
+    protected double amount;
+
+    public Donation(String donorName, double amount) {
+        this.donorName = donorName;
+        this.amount = amount;
+    }
+
+    public String getDonorName() {
+        return donorName;
+    }
+
+    public void setDonorName(String donorName) {
+        this.donorName = donorName;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public abstract void recordDonation();
+}
